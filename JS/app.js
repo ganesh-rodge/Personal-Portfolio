1da3eaa2b8ce24518dc74015cb3e1c0b629ac19c
Ganesh Rodge <ganeshrodge25@gmail.com>
2024-10-08 20:40:49 +0530

    no animation done

--- JS/app.js
+++ JS/app.js
@@ -2,3 +2,19 @@ function toggleMenu(){
     const nav = document.querySelector("#navlinks");
     nav.classList.toggle('active');
 }
+
+function toggleHighlight(){
+    const highl = document.querySelectorAll(".high");
+
+    highl.forEach(element=>{
+        element.classList.toggle('highlight');
+    });
+
+    const highButton = document.querySelector(".highlight-button");
+
+    highButton.classList.toggle('highlight-toggle');
+
+    const hCircle = document.querySelector(".circle");
+    hCircle.classList.toggle('changeCircle');
+}
+
